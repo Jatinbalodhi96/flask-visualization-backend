b21f173b08931a088b9fd21f37bd493e77a9253b
Jatin Balodhi <jatin.balodhi@gramener.com>
2019-02-15 01:04:47 +0530

    FIX: changed sample test

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,2 @@
-def pytest_load_initial_conftests(args):
-    if "xdist" in sys.modules:  # pytest-xdist plugin
-        import multiprocessing
-
-        num = max(multiprocessing.cpu_count() / 2, 1)
-        args[:] = ["-n", str(num)] + args
+def pytest_load_initial_conftests():
+    pass
